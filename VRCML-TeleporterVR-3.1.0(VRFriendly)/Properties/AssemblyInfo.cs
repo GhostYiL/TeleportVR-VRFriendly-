2e4994ae9872612ccf4279fe4d887bf049a0fc1c
GhostYiL <51120128+GhostYiL@users.noreply.github.com>
2019-10-08 15:13:49 +0300

    Updated.Now work with others mods from VRCTools.
    Thats all at this point ¯\_(ツ)_/¯

---
+++ VRCML-TeleporterVR-3.1.0(VRFriendly)/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("3.1.1")]
+[assembly: AssemblyTitle("TeleportVR-VRFriendly")]
+[assembly: AssemblyCopyright("Moddel BY GhostiL (original idea)=>Janni9009")]
