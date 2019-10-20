0d8f08a821303cd57be371f186834d861bff11df
GhostYiL <51120128+GhostYiL@users.noreply.github.com>
2019-10-20 13:41:10 +0300

    Add files via upload

---
+++ VRCML-TeleporterVR/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyTitle("TeleportVR-VRFriendly")]
+[assembly: AssemblyCopyright("Modded BY GhostiL (original idea)=>Janni9009")]
+[assembly: Guid("703287E2-4E03-441E-B3BA-84BDE4305A8C")]
